c2c48454ce1ba61f4b6e16840398d671eb7c7aba
viictoryraves <manfer.alonso@gmail.com>
2026-04-27 20:56:49 +0200

    feat: improve code lesson 12

--- functional-programming/recursion/l12.py
+++ functional-programming/recursion/l12.py
@@ -4,11 +4,11 @@ from typing import List
 def list_files(parent_directory, current_filepath="") -> List:
     file_list = []
 
-    for key in parent_directory.keys():
+    for key, value in parent_directory.items():
         new_filepath = current_filepath + "/" + key
-        if parent_directory[key] is None:
+        if not value:
             file_list.append(new_filepath)
         else:
-            file_list.extend(list_files(parent_directory[key], new_filepath))
+            file_list.extend(list_files(value, new_filepath))
 
     return sorted(file_list)
